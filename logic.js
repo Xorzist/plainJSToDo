e54951fe7e91548be5c796b934f117ee5f4c37a9
rhadjni12 <38302943+Xorzist@users.noreply.github.com>
2023-06-06 00:11:42 -0500

    Updated impl for plain js todo app

--- logic.js
+++ logic.js
@@ -1,118 +1,20 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const title = document.querySelector("#new-task-title");
-  const input = document.querySelector("#new-task-input");
-  const task_list = document.querySelector("#tasks");
- 
+// Initial fetch to get all tasks
+window.onload = function () {
+  getAllTasks();
+};
 
-   loadAllTasks();
+// Function to edit a task
+function editTask(id) {
+  const newTitle = prompt('Enter the new title:');
+  const newDescription = prompt('Enter the new description:');
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    createTask(title.value, input.value);
-    // addElements(title.value,input.value,task_list);
-    
-    window.location.reload();
-  });
-  
- 
-});
-
-function addElements(todo, task_list) {
-  const tasks = document.createElement("div");
-  tasks.classList.add("task");
-  title = `${todo.title}` 
-  description = `${todo.description}`
-
-  const task_content = document.createElement("div");
-  task_content.classList.add("content");
-
-  tasks.appendChild(task_content);
-
-  const task_input = document.createElement("input");
-  task_input.classList.add("text");
-  task_input.type = "text";
-  task_input.setAttribute("readonly", "readonly");
-
-  const task_functions = document.createElement("div");
-  task_functions.classList.add("actions");
-
-  const edit_task = document.createElement("button");
-  edit_task.classList.add("edit");
-  edit_task.innerText = "Edit";
-
-  const delete_task = document.createElement("button");
-  delete_task.classList.add("delete");
-  delete_task.innerText = "Delete";
-
-  const complete_task = document.createElement("button");
-  complete_task.classList.add("completed");
-  complete_task.innerText = "Done";
-
-  task_input.value = title + " : " + description;
-  task_input.setAttribute("readonly", "readonly");
-
-  task_content.appendChild(task_input);
-
-  task_list.appendChild(tasks);
-
-  task_functions.appendChild(complete_task);
-  task_functions.appendChild(edit_task);
-  task_functions.appendChild(delete_task);
-
-  tasks.appendChild(task_functions);
-
-  edit_task.addEventListener("click", (e) => {
-    if (complete_task.innerText.toLowerCase() != "done") {
-      return;
-    }
-    if (edit_task.innerText.toLowerCase() == "edit") {
-      edit_task.innerText = "Save";
-      task_input.removeAttribute("readonly");
-      task_input.focus();
-    } else {
-      edit_task.innerText = "Edit";
-      console.log(" e details : ",e)
-      updateTaskById()
-      task_input.setAttribute("readonly", "readonly");
-    }
-  });
-
-  delete_task.addEventListener("click", (e) => {
-    task_list.removeChild(tasks);
-  });
-
-  complete_task.addEventListener("click", (e) => {
-    if (edit_task.innerText.toLowerCase() == "save") {
-      return;
-    }
-    if (complete_task.innerText.toLowerCase() == "done") {
-      task_input.style.textDecoration = "line-through";
-      task_input.style.color = "red";
-      complete_task.innerText = "Incomplete";
-    } else {
-      task_input.style.textDecoration = "none";
-      task_input.style.color = "whitesmoke";
-      complete_task.innerText = "Done";
-    }
-  });
+  updateTaskById(id, newTitle, newDescription);
 }
 
-function loadAllTasks() {
-  const task_list = document.querySelector("#tasks");
-  
-  getAllTasks()
-    .then((data) => {
-      console.log("API Response: ", data);
-
-      data.forEach((todo) => {
-        task = `${todo.title} : ${todo.description}`;
-        addElements(todo, task_list);
-        console.log(todo);
-      });
-      
-    })
-    .catch((error) => {
-      console.error("Error retrieving tasks:", error);
-    });
+// Function to delete a task
+function deleteTask(id) {
+  const confirmDelete = confirm('Are you sure you want to delete this task?');
+  if (confirmDelete) {
+    deleteTaskById(id);
+  }
 }
--- trigger-apis.js
+++ trigger-apis.js
@@ -1,92 +1,105 @@
-// Function to create a new task
-
 const port = 3001;
-function createTask(title, description) {
-    const task = { title, description };
-  
-  
-    fetch('http://localhost:'+port+'/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(task),
+
+// Function to create a new task
+function createTask() {
+  const title = document.getElementById('task-title').value;
+  const description = document.getElementById('task-description').value;
+
+  const task = { title, description };
+
+  fetch(`http://localhost:${port}/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('New task created:', data);
+      getAllTasks();
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('New task created:', data);
-      })
-      .catch((error) => {
-        console.error('Error creating task:', error);
-      });
-  }
-  
-  // Function to get all tasks
-  function getAllTasks() {
-    return fetch('http://localhost:'+ port+'/tasks')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('All tasks:', data);
-        return data;
-      })
-      .catch((error) => {
-        console.error('Error retrieving tasks:', error);
-      });
-  }
-  
-  // Function to get a specific task by ID
-  function getTaskById(id) {
-    fetch('http://localhost:'+port+'/tasks/${id}')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Task by ID:', data);
-      })
-      .catch((error) => {
-        console.error('Error retrieving task:', error);
-      });
-  }
-  
-  // Function to update a specific task by ID
-  function updateTaskById(id, title, description) {
-    const task = { title, description };
-  
-    fetch('http://localhost:'+port+'/tasks/${id}', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(task),
+    .catch((error) => {
+      console.error('Error creating task:', error);
+    });
+}
+
+// Function to get all tasks
+function getAllTasks() {
+  fetch(`http://localhost:${port}/tasks`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('All tasks:', data);
+      displayTasks(data);
     })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Updated task:', data);
-      })
-      .catch((error) => {
-        console.error('Error updating task:', error);
-      });
-  }
-  
-  // Function to delete a specific task by ID
-  function deleteTaskById(id) {
-    fetch('http://localhost:'+port+'/tasks${id}', {
-      method: 'DELETE',
+    .catch((error) => {
+      console.error('Error retrieving tasks:', error);
+    });
+}
+
+// Function to display tasks in the HTML
+function displayTasks(tasks) {
+  const taskList = document.getElementById('task-list');
+  taskList.innerHTML = '';
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <span>${task.title}: ${task.description}</span>
+      <button onclick="editTask(${task.id})">Edit</button>
+      <button onclick="deleteTask(${task.id})">Delete</button>
+    `;
+    taskList.appendChild(listItem);
+  });
+}
+
+// Function to get a specific task by ID
+function getTaskById(id) {
+  fetch(`http://localhost:${port}/tasks/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Task by ID:', data);
     })
-      .then((response) => {
-        if (response.status === 204) {
-          console.log('Task deleted successfully');
-        } else {
-          console.error('Error deleting task');
-        }
-      })
-      .catch((error) => {
-        console.error('Error deleting task:', error);
-      });
-  }
-  
-  // Call the functions
-//   createTask('Task 1', 'This is task 1');
-//   getAllTasks();
-//   getTaskById(1);
-//   updateTaskById(1, 'Updated Task 1', 'This task has been updated');
-//   deleteTaskById(1);
-  
+    .catch((error) => {
+      console.error('Error retrieving task:', error);
+    });
+}
+
+// Function to update a specific task by ID
+function updateTaskById(id, title, description) {
+  const task = { title, description };
+
+  fetch(`http://localhost:${port}/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Updated task:', data);
+      getAllTasks();
+    })
+    .catch((error) => {
+      console.error('Error updating task:', error);
+    });
+}
+
+// Function to delete a specific task by ID
+function deleteTaskById(id) {
+  fetch(`http://localhost:${port}/tasks/${id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => {
+      if (response.status === 204) {
+        console.log('Task deleted successfully');
+        getAllTasks();
+      } else {
+        console.error('Error deleting task');
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting task:', error);
+    });
+}
