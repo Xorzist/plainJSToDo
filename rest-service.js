49ab564e7a96ec0039ac1aa323a01b81c7b96d71
rhadjni12 <38302943+Xorzist@users.noreply.github.com>
2023-06-06 00:16:27 -0500

    update mongodb connection

--- rest-service.js
+++ rest-service.js
@@ -9,6 +9,28 @@ app.use(cors())
 
 app.use(bodyParser.json());
 
+//MongoDB Connection 
+
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/your-app-name', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+  //Schema
+  const taskSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+  });
+// Task Model
+  const Task = mongoose.model('Task', taskSchema);
+
+
 // In-memory storage for tasks
 let tasks = [
   {id:0,title:"demo",
@@ -21,76 +43,85 @@ description:"demo2"}
 app.post('/tasks', (req, res) => {
   const { title, description } = req.body;
 
-  // Generate a unique ID for the new task
-  const taskId = tasks.length + 1;
-
-  // Create the task object
-  const newTask = {
-    id: taskId,
-    title,
-    description,
-  };
-
-  // Add the task to the in-memory storage
-  tasks.push(newTask);
-
-  res.status(201).json(newTask);
+  // Create a new task using the Task model
+  Task.create({ title, description })
+    .then((newTask) => {
+      res.status(201).json(newTask);
+    })
+    .catch((error) => {
+      console.error('Error creating task:', error);
+      res.status(500).json({ error: 'Server error' });
+    });
 });
+
 
 // Endpoint to get all tasks
 app.get('/tasks', (req, res) => {
-  res.json(tasks);
+  Task.find()
+    .then((tasks) => {
+      res.json(tasks);
+    })
+    .catch((error) => {
+      console.error('Error retrieving tasks:', error);
+      res.status(500).json({ error: 'Server error' });
+    });
 });
 
 // Endpoint to get a specific task by ID
 app.get('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
+  const taskId = req.params.id;
 
-  // Find the task with the matching ID
-  const task = tasks.find((task) => task.id === taskId);
-
-  if (!task) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    res.json(task);
-  }
+  Task.findById(taskId)
+    .then((task) => {
+      if (!task) {
+        res.status(404).json({ error: 'Task not found' });
+      } else {
+        res.json(task);
+      }
+    })
+    .catch((error) => {
+      console.error('Error retrieving task:', error);
+      res.status(500).json({ error: 'Server error' });
+    });
 });
 
 // Endpoint to update a specific task by ID
 app.put('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
+  const taskId = req.params.id;
   const { title, description } = req.body;
 
-  // Find the task with the matching ID
-  const task = tasks.find((task) => task.id === taskId);
-
-  if (!task) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    // Update the task's title and description
-    task.title = title;
-    task.description = description;
-
-    res.json(task);
-  }
+  Task.findByIdAndUpdate(taskId, { title, description }, { new: true })
+    .then((task) => {
+      if (!task) {
+        res.status(404).json({ error: 'Task not found' });
+      } else {
+        res.json(task);
+      }
+    })
+    .catch((error) => {
+      console.error('Error updating task:', error);
+      res.status(500).json({ error: 'Server error' });
+    });
 });
 
 // Endpoint to delete a specific task by ID
 app.delete('/tasks/:id', (req, res) => {
-  const taskId = parseInt(req.params.id);
+  const taskId = req.params.id;
 
-  // Find the index of the task with the matching ID
-  const taskIndex = tasks.findIndex((task) => task.id === taskId);
-
-  if (taskIndex === -1) {
-    res.status(404).json({ error: 'Task not found' });
-  } else {
-    // Remove the task from the in-memory storage
-    tasks.splice(taskIndex, 1);
-
-    res.sendStatus(204);
-  }
+  Task.findByIdAndDelete(taskId)
+    .then((task) => {
+      if (!task) {
+        res.status(404).json({ error: 'Task not found' });
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting task:', error);
+      res.status(500).json({ error: 'Server error' });
+    });
 });
+
 
 // Start the server
 const port = 3001;
